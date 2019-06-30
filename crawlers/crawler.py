e11f25a1688e3e4e4ed864e054680fb25559d70e
erickrus <hyinghao@hotmail.com>
2019-06-29 20:06:39 -0700

    updated crawler

--- crawlers/crawler.py
+++ crawlers/crawler.py
@@ -1,70 +1,62 @@
 import os
+import sys
 
 from image_crawler import ImageCrawler
 from image_downloader import ImageDownloader
 
 SEP = '-' * 70 + '\n'
-
-# ---------------------------------------------------
-# Basic settings for ImageCrawler and ImageDownloader
-# ---------------------------------------------------
-
-print(SEP + 'Basic settings for ImageCrawler and ImageDownloader\n' + SEP)
-
-n_scroll = 5
-keyword = 'panda'
-
-link_save_dir = os.path.join('../data/links', keyword)
-image_save_dir = os.path.join('../data/images', keyword)
-
-print('Keyword:', keyword)
-print('Number of scrolling:', n_scroll)
-print('Links saved in:', link_save_dir)
-print('Images saved in:', image_save_dir)
-print()
-
-# ----------------------------------
-# Save images' links by ImageCrawler
-# ----------------------------------
-
-print(SEP + "Save images' links by ImageCrawler\n" + SEP)
-
-# Search images in baidu
+n_scroll = 2
 engine = 'baidu'
-baidu_links_name = 'baidu_links.csv'
 
-baidu_ic = ImageCrawler(engine)
-baidu_ic.run(keyword, n_scroll)
-baidu_ic.save_links(link_save_dir, baidu_links_name)
+if len(sys.argv)<2:
+    print("Usage: python3.x crawler.py keyword")
+    exit()
 
-# Search images in bing
-engine = 'bing'
-bing_links_name = 'bing_links.csv'
-
-bing_ic = ImageCrawler(engine)
-bing_ic.run(keyword, n_scroll)
-bing_ic.save_links(link_save_dir, bing_links_name)
-
-# Search images in google
-engine = 'google'
-google_links_name = 'google_links.csv'
-
-# google_ic = ImageCrawler(engine)
-# google_ic.run(keyword, n_scroll)
-# google_ic.save_links(link_save_dir, google_links_name)
+keyword = sys.argv[1]
 
 
-print("Images' links are saved in: " + link_save_dir + '\n')
+def crawl(keyword, n_scroll, engine='baidu'):
+    # ---------------------------------------------------
+    # Basic settings for ImageCrawler and ImageDownloader
+    # ---------------------------------------------------
+    print(SEP + 'Basic settings for ImageCrawler and ImageDownloader\n' + SEP)
 
+    link_save_dir = os.path.join('../data/links', keyword)
+    image_save_dir = os.path.join('../data/images', keyword)
 
-# ------------------------------
-# Save images by ImageDownloader
-# ------------------------------
+    print('Keyword:', keyword)
+    print('Number of scrolling:', n_scroll)
+    print('Links saved in:', link_save_dir)
+    print('Images saved in:', image_save_dir)
+    print()
 
-print(SEP + 'Save images by ImageDownloader\n' + SEP)
+    # ----------------------------------
+    # Save images' links by ImageCrawler
+    # ----------------------------------
 
-# Download images to directory
-ider = ImageDownloader(link_save_dir)
-ider.run(image_save_dir)
+    print(SEP + "Save images' links by ImageCrawler\n" + SEP)
 
-print('Images are saved in: ' + image_save_dir + '\n')
+    # Search images in baidu
+    links_name = '%s_links.csv' % engine
+
+    ic = ImageCrawler(engine)
+    ic.run(keyword, n_scroll)
+    ic.save_links(link_save_dir, links_name)
+
+    print("Images' links are saved in: " + link_save_dir + '\n')
+
+    # ------------------------------
+    # Save images by ImageDownloader
+    # ------------------------------
+
+    print(SEP + 'Save images by ImageDownloader\n' + SEP)
+
+    # Download images to directory
+    ider = ImageDownloader(link_save_dir)
+    ider.run(image_save_dir)
+
+    print('Images are saved in: ' + image_save_dir + '\n')
+
+if __name__ == "__main__":
+    crawl(keyword, n_scroll, 'baidu')
+
